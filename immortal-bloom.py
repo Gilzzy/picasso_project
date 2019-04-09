9857a3e4594e3ea527a450ee5648e73386d778ed
Gilzzy <gilzzy1991@hotmail.com>
2019-04-09 23:16:31 +0100

    Added the color changing with movement feature.

--- immortal-bloom.py
+++ immortal-bloom.py
@@ -1,9 +1,19 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
+blue = ( 0, 0, 50)
+yellow = ( 0, 50, 0)
 
 while True:
-  sense.show_message("Immortal Bloom has Begun!", text_colour=yellow, back_colour=blue, scroll_speed=0.05)
+    acceleration = sense.get_accelerometer_raw()
+
+    x = round(acceleration['x'],3)
+    y = round(acceleration['y'],3)
+
+    b1 = int(round(x*255,0))
+    b2 = int(round(y*255,0))
+    blue = ( b2, 0, b1)
+
+
+    sense.show_message("x".format(x), text_colour=yellow, back_colour=blue, scroll_speed=0.01)
 
