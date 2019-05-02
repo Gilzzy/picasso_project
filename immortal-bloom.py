3ba5c9576ce9c156eb42907e36e6e9901c560396
Gilzzy <gilzzy1991@hotmail.com>
2019-05-02 22:50:04 +0100

    Single pixel control

--- immortal-bloom.py
+++ immortal-bloom.py
@@ -1,4 +1,5 @@
 from sense_hat import SenseHat
+import time
 sense = SenseHat()
 
 blue = ( 0, 0, 50)
@@ -12,8 +13,22 @@ while True:
 
     b1 = int(round(x*255,0))
     b2 = int(round(y*255,0))
-    blue = ( b2, 0, b1)
+    blue = (255, 255, 255)
+    
+    for x in range(0,8):
+        for y in range(0,8):
+            sense.set_pixel(x, y, (0,0,255))
+            time.sleep(0.050)
+           # print(x)
+           # print(y)
+    sense.clear()
 
+    for x in range(0,255):
+        for y in range(0,255):
+            sense.set_pixel(0,0, (x,0,y))
+            time.sleep(0.001)
 
-    sense.show_message("x".format(x), text_colour=yellow, back_colour=blue, scroll_speed=0.01)
+    sense.clear()
+    
+    #    sense.show_message("x".format(x), text_colour=yellow, back_colour=blue, scroll_speed=0.01)
 
